ca7de3bbb3ab92348d9f6de8066811d59fbaf65f
Vikrant Singh <vikrant.singh@us.abb.com>
2022-02-21 16:09:31 -0600

    Others folder for practice files

---
+++ Others/Learning_Python.py
@@ -0,0 +1,34 @@
+# # How many votes did you get?
+# my_votes = int(input("How many votes did you get in the election? "))
+# #  Total votes in the election
+# total_votes = int(input("What is the total votes in the election? "))
+# # Calculate the percentage of votes you received.
+# percentage_votes = (my_votes / total_votes) * 100
+# print("I received " + str(percentage_votes)+"% of the total votes.")
+
+# counties = ["Arapahoe","Denver","Jefferson"]
+# if counties[1] == 'Denver':
+#     print(counties[1])
+
+# temperature = int(input("What is the temperature outside? "))
+
+# if temperature > 80:
+#     print("Turn on the AC.")
+# else:
+#     print("Open the windows.")
+
+# What is the score?
+# score = int(input("What is your test score? "))
+
+# # Determine the grade.
+# if score >= 90:
+#     print('Your grade is an A.')
+# elif score >= 80:
+#     print('Your grade is a B.')
+# elif score >= 70:
+#     print('Your grade is a C.')
+# elif score >= 60:
+#     print('Your grade is a D.')
+# else:
+#     print('Your grade is an F.')
+
